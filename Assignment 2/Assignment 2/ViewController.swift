65abc45f91c6fb13ff3d57dfd6b2ffa62b5206d6
user169155 <user169155@74-80-245-9.ic2net.net>
2020-04-17 15:38:13 -0700

    2020-04-17

---
+++ Assignment 2/Assignment 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by user169155 on 4/17/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
